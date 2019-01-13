852b08657abd335f4c5d7345f9bd0c69fd8fdf36
Rashed Rahman Suhan <41446985+rashedsuhan@users.noreply.github.com>
2019-01-13 20:49:25 +0600

    Add files via upload

---
+++ Adding two integers.c
@@ -0,0 +1,15 @@
+// Adding two integers
+#include <stdio.h>
+
+int main (void)
+{
+    int integer1;
+    int integer2;
+    int sum;
+    printf( "Enter first integer\n" );
+    scanf("%d", &integer1 );
+    printf ( "Enter second integer\n" );
+    scanf ("%d", &integer2);
+    sum = integer1 + integer2;
+    printf("The sum is %d\n", sum);
+}

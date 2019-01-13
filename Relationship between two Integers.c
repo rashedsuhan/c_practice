923ae17d94788330685b9c9b28fa2625fa070c83
Rashed Rahman Suhan <41446985+rashedsuhan@users.noreply.github.com>
2019-01-13 22:09:01 +0600

    Add files via upload

---
+++ Relationship between two Integers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main ()
+{
+    int num1; int num2;
+    printf("Enter two integers, and I will tell you the relationships they satisfy:\n");
+    scanf("%d%d", &num1, &num2);
+    if ( num1 == num2)
+    {
+        printf("%d is equal to %d\n", num1, num2);
+    }
+    if ( num1 != num2)
+    {
+        printf("%d is not equal to %d\n", num1, num2);
+    }
+    if ( num1 > num2 )
+    {
+        printf("%d is greater than %d\n",num1,num2);
+    }
+    if ( num1 < num2)
+    {
+        printf("%d is less than %d\n", num1,num2);
+    }
+
+}

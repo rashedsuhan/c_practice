c4ca855352cfb3592e7276b1c6b74d7c72ecc95e
Rashed Rahman Suhan <41446985+rashedsuhan@users.noreply.github.com>
2019-01-15 19:40:22 +0600

    Add files via upload

---
+++ C source codes/labclass2_DaysMonth.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int day; int month;
+    printf("Enter the number of days:\n");
+    scanf("%d", &day);
+    printf("Month = %d\n", month = day / 30 );
+    printf("Days = %d\n", day = day % 30);
+    return main();
+}
